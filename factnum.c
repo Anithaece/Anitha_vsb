d27dcf80496437d6cb2d7ae91a197fbcf64c4fd0
Anithaece <30368535+Anithaece@users.noreply.github.com>
2017-08-15 19:09:59 +0530

    Update factnum.c

--- factnum.c
+++ factnum.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 int main()
 {
-int i,n,fact=1;
+int j,n,f=1;
 printf("Enter the number: \n");
 scanf("%d",&n);
-for(i=1;i<=n;i++)
-fact=fact*i;
-printf("factorial of the number %d is %d",n,fact);
+for(j=1;j<=n;j++)
+f=f*j;
+printf("factorial of the number %d is %d",n,f);
 return 0;
 }
